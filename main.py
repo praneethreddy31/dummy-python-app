7a948702c309d0f7614ca644df998d7fb8f349a6
praneethreddy31 <praneethreddy31@users.noreply.github.com>
2025-09-02 17:45:46 +0530

    fix: Correct main.py formatting

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-from fastapi import FastAPI\n\napp = FastAPI()\n\n@app.get("/")\ndef read_root():\n    return {"Hello": "World"}
+from fastapi import FastAPI\n\napp = FastAPI()\n\n@app.get("/")\ndef read_root():\n    return {"Hello": "World"}\n
